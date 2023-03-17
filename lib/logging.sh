0753973ceca10e0c216956d25b36ca08516e38dd
vgitv <33827923+vgitv@users.noreply.github.com>
2023-03-17 14:04:07 +0100

    logging lib

---
+++ lib/logging.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export NC="\033[0m"
+
+export debug="\033[1;30m"
+export info="\033[1;32m"
+export warn="\033[1;33m"
+export error="\033[1;31m"
+
+
+function log {
+    echo -e "${!1:?}$(date '+%Y-%m-%d %H:%M:%S') - $(printf "%-5s" "${1^^}") - ${2}${NC}"
+}
+
+
+function error {
+    log error "$*"
+    exit 1
+}
