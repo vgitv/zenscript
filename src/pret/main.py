60e4d8dad374ba9e514017ad6e83d2552430e805
vgitv <33827923+vgitv@users.noreply.github.com>
2023-03-17 14:02:41 +0100

    merge

--- src/pret/main.py
+++ src/pret/main.py
@@ -2,7 +2,9 @@
 
 from Pret import *
 
-testPret = Pret(100000, 0.02, 10)
-testPret.build()
-testPret.graph()
+testPret = Pret(200000, 0.0185, 20, 0.008)
 
+testPret.build()
+print(testPret)
+testPret.graph()
+testPret.graph2()
