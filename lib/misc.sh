7d6e771784e4b2dd8da4965ed220db8eb7d0dcfd
vgitv <33827923+vgitv@users.noreply.github.com>
2023-03-17 14:04:06 +0100

    retry function in misc lib

--- lib/misc.sh
+++ lib/misc.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 set -euo pipefail
 
-function title
-{
+function title {
     col=155
     echo -e "\e[38;5;${col}m$1\e[0m"
+}
+
+function try {
+    local action='retry'
+
+    echo '# --------------------------------'
+    echo "# $*"
+
+    while [[ "${action}" = 'retry' ]]; do
+        if "$@"; then
+            echo 'OK'
+        else
+            echo "ERREUR"
+            echo 'Continue ? ( retry / skip )'
+            read -r action
+            while [[ ! "${action}" =~ (retry|skip) ]]; do
+                echo 'Wrong choice ( retry / skip )'
+                read -r action
+            done
+        fi
+    done
+    echo
 }
